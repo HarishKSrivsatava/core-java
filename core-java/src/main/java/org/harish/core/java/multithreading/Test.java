f6ca41b2fa14b13492d3ec5d10115fa2a5bb22e8
Harish Kumar <harishkumar9758@gmail.com>
2021-07-04 16:14:05 +0530

    Test

---
+++ core-java/src/main/java/org/harish/core/java/multithreading/Test.java
@@ -0,0 +1,8 @@
+package org.harish.core.java.multithreading;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+}
---
+++ core-java/src/main/java/org/harish/core/java/polymorphism/StaticControlFlowDemo.java
@@ -0,0 +1,24 @@
+package org.harish.core.java.polymorphism;
+
+public class StaticControlFlowDemo {
+
+	/*
+	public static int x = 10;
+	static {
+		m();
+		System.out.println("First Static Block");
+	}*/
+	public static void main(String[] args) {
+		// m();
+		 System.out.println("Main method");
+	}
+	/*
+	public static void m() {
+		System.out.println(y);
+	}
+	static {
+		System.out.println("Second Static Block");
+	}
+	static int y = 20;
+	*/
+}
